00d777a5db0f1c6482c4d60a4dc78e1b5ce82c54
riacosta <rigo93acosta@gmail.com>
2024-07-10 19:57:04 -0300

    Finished Project

--- 29_discord_bot_mine/main.py
+++ 29_discord_bot_mine/main.py
@@ -1,1 +1,34 @@
 from discord import Client, Intents
+import responses
+from api_key import key
+
+def run_bot(token: str):
+    
+    # Basic bot setup
+    intents = Intents.default()
+    intents.message_content = True
+    client = Client(intents=intents)
+
+    knowledge: dict = responses.load_knowledge('29_discord_bot_mine/knowledge.json')
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        if message.author == client.user:
+            return
+
+        if message.content:
+            print(f'({message.channel}) {message.author}: {message.content}')
+            response: str = responses.get_response(message.content, knowledge=knowledge)
+            await message.channel.send(response)
+
+        else:
+            print('!!! Carepinga usa las palabras correctas !!!!')
+
+    client.run(token=token)
+
+if __name__ == "__main__":
+    run_bot(token=key)
